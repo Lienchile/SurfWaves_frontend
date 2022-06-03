fa912d8e779fcd6eb1be022d5cd827ffc3270729
Thi Lienchi Le <lienchi.le2@gmail.com>
2022-06-03 15:34:07 +0200

    updated front-end by improve the image position

--- app.py
+++ app.py
@@ -2,12 +2,9 @@
 import streamlit as st
 from datetime import date
 from datetime import datetime
-
 import pandas as pd
-import datetime as dt
 
-
-### SURF WAVES STREAMLIT CODE ###
+### --------------SURF WAVES STREAMLIT CODE -------------------------###
 
 # ------------------------Header Section-----------------------------#
 
@@ -38,9 +35,9 @@ elif api_input == 'Oostend':
     st.sidebar.image('oostend.jpg',use_column_width='always')
 else:
     st.sidebar.write(f'You choose {api_input}')
-    st.sidebar.write('''Knokke is the most north-eastern seaside resort on the Belgian coast. It lies adjacent to the Dutch border;
-                     separated from the Dutch territory by the Zwin nature reserve. Knokke came into existence as a result
-                     of the construction of dikes that were to protect the area around the 'Zwin' sea-arm.''')
+    st.sidebar.write('''Knokke is the most north-eastern seaside resort on the Belgian coast.
+                     It lies adjacent to the Dutch border;
+                     separated from the Dutch territory by the Zwin nature reserve.''')
     st.sidebar.image('knokke.jpg',use_column_width='always')
 
 
@@ -51,18 +48,19 @@ col1, col2 = st.columns(2)
 
 # --------------------- Left Column Section/Column1------------------#
 
+#Add a time stamp selection
+now = datetime.now()
+timestamp =pd.to_datetime(now)
+# Print the current date only
+today = date.today()
+
 #Dummy answer
-rating=1
-wind_speed = 35
-wind_direction = 'N'
-tide = 'low'
-wave_height = 350
 
 api = {"rating": 2,
        'wind_speed': 10.5,
-       'wind_direction':wind_direction,
+       'wind_direction':58,
         'tide': 'low',
-        'wave_height': 15}
+        'wave_height': 150}
 
 if st.sidebar.button('Click to see the result'):
     # print is visible in the server output, not in the page
@@ -72,10 +70,11 @@ if st.sidebar.button('Click to see the result'):
         expander = col1.expander("Optional informations")
         with expander:
             col1.metric(label="The wind speed in m/s", value=api['wind_speed'])
-            col1.metric(label="The wave height in m", value=api['wave_height'])
+            col1.metric(label="The wave height in cm", value=api['wave_height'])
             col1.metric(label="The tide is", value=api['tide'])
-            col1.image("surfeur.jpg")
+            col2.image("surfeur.jpg")
         st.success('It is the ideal time to surf!')
+        st.write(f'''Date: {today} - Time: {now.hour}:{now.minute} ''')
     else:
         col1.snow()
         expander = col1.expander("Optional informations")
@@ -84,38 +83,9 @@ if st.sidebar.button('Click to see the result'):
             col1.metric(label="The wave height in m", value=api['wave_height'])
             col1.metric(label="The tide is", value=api['tide'])
             col1.write('')
-            col1.image("home.jpg")
+            col2.image("home.jpg")
         st.error('Mmmh.. maybe you should stay home')
+        st.write(f'''Date: {today} - Time: {now.hour}:{now.minute} ''')
 
 else:
-    col1.write('Click on the button to see if it is a good day to go surfing')
-
-
-#Add a time stamp selection
-now = datetime.now()
-timestamp =pd.to_datetime(now)
-# Print the current date only
-today = date.today()
-st.write(f'''
-      Date: {today} - Time: {now.hour}:{now.minute} ''')
-
-
-# --------------------- Right Column Section/Column2------------------#
-
-# @st.cache
-# def get_map_data():
-
-#     return pd.DataFrame(
-#             [[51.228443,2.934465],[51.346352,3.285860],[51.0955,2.5874]],
-#             columns=['lat', 'lon']
-#         )
-
-coord = pd.DataFrame({
-    'awesome cities' : ['Oostend','Knokke', 'De Panne'],
-    'lat' : [51.228443, 51.346352,  51.0955],
-    'lon' : [2.934465, 3.285860, 2.5874]
-})
-height=500
-width=500
-
-col2.map(coord,zoom=8,)
+    st.write('👈 Choose your location and click on the button on the sidebar to see if it is a good day to go surfing')
